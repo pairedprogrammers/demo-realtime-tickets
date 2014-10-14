635ab5fad3c1a5f5041f9461d201b6e1ef989ff2
Paul Welden <paul.j.welden@gmail.com>
2014-10-13 21:30:35 -0500

    adjusted some simple client stuff

--- app/js/Tickets.js
+++ app/js/Tickets.js
@@ -1,42 +1,47 @@
 (function () {
     'use strict';
-    var module = angular.module ('realtimeData.data', []);
+    var module = angular.module('realtimeData.data', []);
 
-    module.factory ('Tickets', Tickets);
+    module.factory('Tickets', Tickets);
 
     Tickets.$inject = [];
-    
-    function Tickets () {
+
+    function Tickets() {
         var tickets = [
             {
-                'id': 1,
                 'title': 'The network is down!',
-                'desc': 'The network is down and I cannot perform my work',
-                'severity': 'Critical'
+                'desc': 'The network is down and I cannot perform my work'
             },
             {
-                'id': 2,
                 'title': 'I lost my pen',
-                'desc': 'I was near my cube when I noticed that my pen was gone!  Someone must have taken it.',
-                'severity': 'low'
+                'desc': 'I was near my cube when I noticed that my pen was gone!  Someone must have taken it.'
             },
             {
-                'id': 3,
                 'title': 'PC Load Letter',
-                'desc': 'I wish I knew what it meant, but the printer is not working.',
-                'severity': 'Critical'
+                'desc': 'I wish I knew what it meant, but the printer is not working.'
             },
             {
-                'id': 4,
                 'title': 'My account is locked',
-                'desc': 'I know that my password was correct even though it failed.  So I kept trying.  Only about 50 times.',
-                'severity': 'low'
+                'desc': 'I know that my password was correct even though it failed.  So I kept trying.  Only about 50 times.'
             }
         ];
         var service = {
             // functions
-            $get: function () { return tickets; }
+            query: query,
+            save: save
         };
+
         return service;
+
+        //////////////////////////////////////////////////////////////
+
+        function query() {
+            return tickets;
+        }
+
+        function save(newTicket) {
+            tickets.push({'title': newTicket.title, 'desc': newTicket.desc});
+        }
+
     }
-}) ();
+})();
