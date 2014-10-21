d385546827b16038e5b6475db10ceff3c9755c58
Keith Ott <ducttape12@yahoo.com>
2014-10-20 19:48:38 -0500

    Node JS serving up a web page

---
+++ app/js/TicketsSocketIO.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('realtimeData.data', ['ngResource']).factory('Tickets', ['$resource', function($resource) {
+    //var socket = io();
+    var server = $resource('/tickets');
+    
+    var tickets = [];
+    
+    /*socket.on('ticket', function(msg) {
+        tickets.push(msg);
+    });*/
+    
+    return {
+        save: function(newTicket) {
+            //socket.emit('ticket', newTicket);
+        },
+        
+        query: function(callback) {
+            return server.query();
+        }
+    };
+}]);
