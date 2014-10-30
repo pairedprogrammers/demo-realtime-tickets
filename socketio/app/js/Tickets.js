dbd492329a8dc503e246352a532af3d32d955be3
Keith Ott <ducttape12@yahoo.com>
2014-10-30 18:55:22 -0500

    Simplifying things

--- socketio/app/js/Tickets.js
+++ socketio/app/js/Tickets.js
@@ -1,47 +1,15 @@
-(function () {
+angular.module('realtimeData.data', ['ngResource']).factory('Tickets', ['$resource', function($resource) {
     'use strict';
-    var module = angular.module('realtimeData.data', []);
-
-    module.factory('Tickets', Tickets);
-
-    Tickets.$inject = [];
-
-    function Tickets() {
-        var tickets = [
-            {
-                'title': 'The network is down!',
-                'desc': 'The network is down and I cannot perform my work'
-            },
-            {
-                'title': 'I lost my pen',
-                'desc': 'I was near my cube when I noticed that my pen was gone!  Someone must have taken it.'
-            },
-            {
-                'title': 'PC Load Letter',
-                'desc': 'I wish I knew what it meant, but the printer is not working.'
-            },
-            {
-                'title': 'My account is locked',
-                'desc': 'I know that my password was correct even though it failed.  So I kept trying.  Only about 50 times.'
-            }
-        ];
-        var service = {
-            // functions
-            query: query,
-            save: save
-        };
-
-        return service;
-
-        //////////////////////////////////////////////////////////////
-
-        function query() {
-            return tickets;
+    
+    var server = $resource('/tickets');
+    
+    return {
+        save: function (newTicket) {
+            server.save(newTicket);
+        },
+        
+        query: function () {
+            return server.query();
         }
-
-        function save(newTicket) {
-            tickets.push({'title': newTicket.title, 'desc': newTicket.desc});
-        }
-
-    }
-})();
+    };
+}]);
