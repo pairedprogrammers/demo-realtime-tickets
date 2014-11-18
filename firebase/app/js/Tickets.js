63f42763ac26e360266972183dcb6996bafa7b5c
Paul Welden <paul.j.welden@gmail.com>
2014-11-17 21:27:10 -0600

    added helper comment

--- firebase/app/js/Tickets.js
+++ firebase/app/js/Tickets.js
@@ -2,7 +2,7 @@ angular.module('realtimeData.data', ['firebase'])
     .factory('Tickets', ['$firebase',  function ($firebase) {
         'use strict';
 
-        var ref = new Firebase('https://realtime-data-demo.firebaseio.com');
+        var ref = new Firebase('https://realtime-data-demo.firebaseio.com'); //***PUT IN YOUR OWN FIREBASE URI HERE****
         return $firebase(ref).$asArray();
 
     }]);
